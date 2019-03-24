b6e88222ccd8af46bf8d349aa01ff450bdae87c6
Mikita Dubovik <Mikita_Dubovik@epam.com>
2019-03-24 12:26:52 +0300

    Task 2

---
+++ Module.Introduction/ViewComponents/BreadcrumbsViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace TestsProject
+{
+    //[ViewComponent(Name = "Breadcrumbs")]
+    public class BreadcrumbsViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(HttpContext context)
+        {
+            string currentPath = "Home" +  context.Request.Path.Value.Replace('/','>'); //context.Request.Path.Value;
+            return View("Default", currentPath);
+        }
+    }
+}
